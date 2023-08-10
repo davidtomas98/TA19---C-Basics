10e2c21b2a2a1e31d5b4bc9b040b9660660686c1
David Tomas <david.tomas@grupocastilla.es>
2023-08-10 16:00:51 +0200

    Modificaciones varias

---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración de variables
+            int num1 = 10;
+            int num2 = 5;
+
+            // Realizar operaciones matemáticas
+            int suma = num1 + num2;
+            int resta = num1 - num2;
+            int multiplicacion = num1 * num2;
+            int division = num1 / num2;
+            int modulo = num1 % num2;
+
+            // Mostrar resultados por consola
+            Console.WriteLine($"Suma: {suma}");
+            Console.WriteLine($"Resta: {resta}");
+            Console.WriteLine($"Multiplicación: {multiplicacion}");
+            Console.WriteLine($"División: {division}");
+            Console.WriteLine($"Módulo: {modulo}");
+        }
+    }
+}
---
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración e inicialización de variables
+            int A = 1, B = 2, C = 3, D = 4;
+
+            // Mostrar los valores iniciales de las variables
+            Console.WriteLine($"Valores iniciales: A = {A}, B = {B}, C = {C}, D = {D}");
+
+            // Realizar el intercambio de valores entre las variables
+            int tempB = B;
+            B = C;
+            C = A;
+            A = D;
+            D = tempB;
+
+            // Mostrar los nuevos valores de las variables después del intercambio
+            Console.WriteLine($"Nuevos valores: A = {A}, B = {B}, C = {C}, D = {D}");
+        }
+    }
+}
